efaa0336fbd4312b2b2ec0bb9035dc9a250819a1
khairnarswapna <khairnarswapna99@gmail.com>
2019-12-17 14:53:20 +0530

    Added: coed and Test for checking equality of inch and feet

--- src/main/java/com/qaulitymeasurment/Length.java
+++ src/main/java/com/qaulitymeasurment/Length.java
@@ -2,7 +2,10 @@ package com.qaulitymeasurment;
 
 public class Length {
 
-    enum Unit {FEET, INCH}
+
+    private static final double FEET_TO_INCH = 12.0;
+
+    enum Unit {FEET, INCH;}
 
     private final Unit unit;
     private final double value;
@@ -10,6 +13,14 @@ public class Length {
     public Length(Unit unit, double value) {
         this.unit = unit;
         this.value = value;
+    }
+
+    public boolean compare(Length thatQuantity) {
+        if (this.unit.equals(Unit.FEET) && thatQuantity.unit.equals(Unit.FEET))
+            return Double.compare(this.value, thatQuantity.value) == 0;
+        if (this.unit.equals(Unit.FEET) && thatQuantity.unit.equals(Unit.INCH))
+            return Double.compare(this.value * FEET_TO_INCH, thatQuantity.value) == 0;
+        return false;
     }
 
     @Override
--- src/test/java/com/qaulitymeasurment/QualityTest.java
+++ src/test/java/com/qaulitymeasurment/QualityTest.java
@@ -40,4 +40,28 @@ public class QualityTest {
         Assert.assertNotEquals(feet, inch);
     }
 
+    @Test
+    public void given0FeetAnd0Inch_ShouldReturnEqualLength() {
+        Length feet = new Length(Length.Unit.FEET, 0.0);
+        Length inch = new Length(Length.Unit.INCH, 0.0);
+        boolean compareCheck = feet.compare(inch);
+        Assert.assertTrue(compareCheck);
+    }
+
+    @Test
+    public void given1FeetAnd1Inch_ShouldReturnNotEqualLength() {
+        Length feet = new Length(Length.Unit.FEET, 1.0);
+        Length inch = new Length(Length.Unit.INCH, 1.0);
+        boolean compareCheck = feet.compare(inch);
+        Assert.assertFalse(compareCheck);
+    }
+
+    @Test
+    public void given1FeetAnd1Feet_ShouldReturnEqualLength() {
+        Length feet1 = new Length(Length.Unit.FEET, 1.0);
+        Length feet2 = new Length(Length.Unit.FEET, 1.0);
+        boolean compareCheck = feet1.compare(feet2);
+        Assert.assertTrue(compareCheck);
+    }
+
 }
